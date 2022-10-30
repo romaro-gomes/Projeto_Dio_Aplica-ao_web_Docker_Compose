3d6361bd03b179c8132ed07e45c37bd082f0b414
romaro <romario.j.gomes@gmail.com>
2022-10-30 12:09:17 -0300

    Primeiro Commit

---
+++ Docker-apache.sh
@@ -0,0 +1,10 @@
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+docker pull httpd
+
+docker-compose up -d
+
+ 
